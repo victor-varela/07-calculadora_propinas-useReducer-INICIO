ae26b22e1460c399699e8a5f48f87ec043813616
Victor Varela <victor398@gmail.com>
2025-03-27 12:30:45 -0300

    first-commit

---
+++ src/App.tsx
@@ -0,0 +1,77 @@
+import { useReducer } from "react"
+import MenuItem from "./components/MenuItem"
+import OrderContents from "./components/OrderContents"
+import OrderTotals from "./components/OrderTotals"
+import TipPercentageForm from "./components/TipPercentageForm"
+import { menuItems } from "./data/db"
+import useOrder from "./hooks/useOrder"
+import { initialState, orderReducer } from "./reducers/order-reducer"
+
+function App() {
+
+  const { order, tip, setTip, addItem, removeItem, placeOrder } = useOrder()
+  
+  const [state, dispatch] = useReducer(orderReducer, initialState)
+
+  return (
+    <>
+        <header className=" bg-teal-400 py-5">
+          <h1 className="text-center text-4xl font-black">Calculadora de Propinas y Consumo</h1>
+        </header>
+
+        <main className=" max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+          <div className='p-5'>
+            <h2 className='font-black text-4xl'>Menú</h2>
+
+            <div className='mt-10 space-y-3'>
+              {menuItems.map(item => (
+                <MenuItem 
+                  key={item.id}
+                  item={item}
+                  addItem={addItem}
+                />
+              ))}
+            </div>
+          </div>
+
+          <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
+            {order.length ? (
+              <>
+                  <OrderContents
+                    order={order}
+                    removeItem={removeItem}
+                  />
+                  <TipPercentageForm 
+                    setTip={setTip}
+                    tip={tip}
+                  />
+                  <OrderTotals 
+                    order={order}
+                    tip={tip}
+                    placeOrder={placeOrder}
+                  />
+              </>
+            ) : (
+              <p className="text-center">La orden esta vacia</p>
+            )}
+ 
+
+          </div>
+        </main>
+
+    </>
+  )
+}
+
+export default App
+
+/*
+  Para pasar de un custom hook a un reducer, primero se crea el archivo use'nombreDelReducer'-reducer.ts
+  Luego se hace el esqueleto del reducer: action, initialState, reducerState --> ver archivo useOrder-reducer.ts
+
+  Despues de creado el esqueleto del reducer. Se crea el reducer aca en App.tsx
+   const [state, action] = useReducer(orderReducer, initialState)
+
+
+
+*/
---
+++ src/reducers/order-reducer.ts
@@ -0,0 +1,69 @@
+import { MenuItem, OrderItem } from "../types";
+
+// paso 1
+export type OrderActions =
+  | { type: "add-item"; payload: { item: MenuItem } }
+  | { type: "remove-item"; payload: { id: MenuItem["id"] } }
+  | { type: "place-order" }
+  | { type: "define-tip"; payload: { value: number } };
+
+// paso 3
+export type OrderState = {
+  order: OrderItem[];
+  tip: number;
+};
+
+// paso 2
+export const initialState: OrderState = {
+  order: [],
+  tip: 0,
+};
+
+export const orderReducer = (state: OrderState = initialState, action: OrderActions) => {
+  if (action.type === "add-item") {
+    return {
+      ...state,
+    };
+  }
+
+  if (action.type === "remove-item") {
+    return {
+      ...state,
+    };
+  }
+
+  if (action.type === "place-order") {
+    return {
+      ...state,
+    };
+  }
+
+  if (action.type === "define-tip") {
+    return {
+      ...state,
+    };
+  }
+
+  return state;
+};
+
+/*
+    Tomamos las funciones del hook y las pasamos al reducer.
+    1.- Crear el type de acciones del reducer
+    2.- Definimos el initialState- Vemos como es el state (cuantos useState usamos en el hook)
+
+        const [order, setOrder] = useState<OrderItem[]>([])
+        const [tip, setTip] = useState(0)
+
+    cuando usamos useState, le asignamos un valor inicial, bueno eso mismo es lo que hacemos en el reducer
+
+    3.- Cascada de tipos: en este punto ya difinimos el initialState, pero NO hemos definido el type de ese initialState, es por eso que volvemos arriba en el codigo y lo definimos. Por ello ves que initialState tiene tipo ' : ' OrderState
+
+    4.- Creamos la funcion reducer, como parametros siempre son state: OrderState (volvemos a indicar el tipo aca) y el segundo parametro es action de tipo OrderACtions. Siempre al final va el return state (ese es por defecto)
+    Cada accion tiene un retrurn{...state} por defecto.
+
+    Asi se arma el esqueleto
+    
+
+
+*/
